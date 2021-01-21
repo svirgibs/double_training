e650b8b88c011a7078d6519b0a0f6bb41c048c1a
Denis Yavorskiy <svirgibs@gmail.com>
2021-01-21 20:34:19 +0300

    Commit

--- addressbook-web-tests/addressbook-web-tests/Appmanager/NavigationHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/Appmanager/NavigationHelper.cs
@@ -20,11 +20,19 @@ namespace WebAddressbookTests
 
         public void GoToHomePage()
         {
+            if (driver.Url == baseURL + "/addressbook/")
+            {
+                return;
+            }
             driver.Navigate().GoToUrl(baseURL + "/addressbook/");
         }
 
         public void GoToGroupsPage()
         {
+            if (driver.Url == baseURL + "/addressbook/group.php" && IsElementPresent(By.Name("new")))
+            {
+                return;
+            }
             driver.FindElement(By.LinkText("groups")).Click();
         }
     }
