b52d01fc38e1ca80cfe729aee6b46fc4dc43f126
Denis Yavorskiy <svirgibs@gmail.com>
2021-01-21 20:25:55 +0300

    Commit

--- addressbook-web-tests/addressbook-web-tests/Appmanager/ApplicationManager.cs
+++ addressbook-web-tests/addressbook-web-tests/Appmanager/ApplicationManager.cs
@@ -49,7 +49,9 @@ namespace WebAddressbookTests
         {
             if (! app.IsValueCreated)
             {
-                app.Value = new ApplicationManager();
+                ApplicationManager newInstance = new ApplicationManager();
+                newInstance.Navigator.GoToHomePage();
+                app.Value = newInstance;
             }
             return app.Value;
         }
--- addressbook-web-tests/addressbook-web-tests/Tests/AuthTestBase.cs
+++ addressbook-web-tests/addressbook-web-tests/Tests/AuthTestBase.cs
@@ -7,14 +7,11 @@ using NUnit.Framework;
 
 namespace WebAddressbookTests
 {
-    [SetUpFixture]
-    public class TestSuiteFixture
+    public class AuthTestBase : TestBase
     {
         [SetUp]
-        public void InitApplicationManager()
+        public void SetupLogin()
         {
-            ApplicationManager app = ApplicationManager.GetInstance();
-            app.Navigator.GoToHomePage();
             app.Auth.Login(new AccountData("admin", "secret"));
         }
     }
